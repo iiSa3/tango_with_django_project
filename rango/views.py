d9b108a13855c83335df9826269efec725b65da4
Sam Griffin <2327827g@student.gla.ac.uk>
2018-02-05 15:49:13 +0000

    Final exercise

--- rango/views.py
+++ rango/views.py
@@ -163,4 +163,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
